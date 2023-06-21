4b49d234d4111c2aa72ccfc73e8d3344d18c593c
tshandu <tshandurammbasa@gmail.com>
2023-06-21 02:23:38 -0700

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,10 +7,10 @@
 void print_times_table(int n)
 {
 int i, j, k;
-
 if (n >= 0 && n <= 15)
 {
 for (i = 0; i <= n; i++)
+{
 for (j = 0; j <= n; j++)
 {
 k = j * i;
@@ -44,5 +44,6 @@ _putchar((k % 10) + '0');
 }
 }
 _putchar('\n');
+}
 }
 }
