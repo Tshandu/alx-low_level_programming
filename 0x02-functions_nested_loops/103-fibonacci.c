a950b3c652ddbb385d17dcf381738bee6f8a0fef
tshandu <tshandurammbasa@gmail.com>
2023-06-21 01:14:25 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,7 +6,6 @@
 * Return: Always 0 (Success)
 */
 int main(void)
-
 {
 int i;
 unsigned long int j, k, next, sum;
@@ -14,6 +13,7 @@ unsigned long int j, k, next, sum;
 j = 1;
 k = 2;
 sum = 0;
+
 for (i = 1; i <= 33; ++i)
 {
 if (j < 4000000 && (j % 2) == 0)
@@ -23,4 +23,9 @@ sum = sum + j;
 next = j + k;
 j = k;
 k = next;
+}
+
+printf("%lu\n", sum);
+
+return (0);
 }
